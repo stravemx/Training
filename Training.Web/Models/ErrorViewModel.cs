a8dec4f991e3d22b4e18ec810f7452b22ad227d2
Jrivera <jump31@hotmail.com>
2020-04-28 23:50:04 -0700

    Estructura Proyecto

---
+++ Training.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Training.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
